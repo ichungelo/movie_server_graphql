ac3c8a2b012cc9d2fea721173c7a6dc0386d0a8d
Krisna Satriadi <krisna.s.362@gmail.com>
2022-04-01 10:48:54 +0700

    fix migration column name

--- pkg/db/migration/mysql/000002_movies_table.up.sql
+++ pkg/db/migration/mysql/000002_movies_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `movies` (
     `id` int NOT NULL AUTO_INCREMENT,
     `title` varchar(64) NOT NULL,
-    `release_year` year DEFAULT NULL,
-    `production` varchar(64) NOT NULL,
+    `year` year DEFAULT NULL,
+    `poster` varchar(64) NOT NULL,
     `overview` text,
     PRIMARY KEY (`id`),
     UNIQUE KEY `id` (`id`)
